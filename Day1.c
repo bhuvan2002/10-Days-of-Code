6b684dee245868ef0bc67cfd23ebaa5fa6a6f85e
bhuvan2002 <bhuvanumesh123@gmail.com>
2021-11-26 23:16:23 +0530

    first commit

---
+++ Day1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num,n,digit,i = 0;
+    int rem;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Enter digit to search: ");
+    scanf("%d",&digit);
+
+    while(n>0)
+    {
+        rem=n%10;
+        if(rem==digit)
+            i++;
+        n/=10;
+    }
+
+    printf("Total occurrence of digit is: %d ",i);
+
+    return 0;
+}
+
